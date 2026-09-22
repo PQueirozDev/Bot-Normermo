723fa61b5d7803dbf5d122dd4e52458367d5bbf9
Pedro Queiroz <pedrohenriqueiroz158@gmail.com>
2026-09-22 18:57:12 -0300

    Adiciona resposta automatica para links do X

---
+++ src/events/guild/messageTwitter.ts
@@ -0,0 +1,17 @@
+﻿import { Events, Message } from "discord.js";
+
+export default {
+  name: Events.MessageCreate,
+
+  async execute(message: Message) {
+    if (message.author.bot) return;
+
+    const xLinkRegex = /https?:\/\/(?:www\.)?x\.com\/\S+/i;
+
+    if (!xLinkRegex.test(message.content)) return;
+
+    await message.reply(
+      "Para de postar hl q tu vai famoso nao seu bosta"
+    );
+  },
+};
